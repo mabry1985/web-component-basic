69cf731453642359bbda86527b73b6de4cc5ebb6
Josh Mabry <mabry1985@gmail.com>
2021-02-07 10:18:22 -0800

    update component to use shadow dom

--- tooltip.js
+++ tooltip.js
@@ -2,24 +2,35 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = 'Some dummy text.';
+    this.attachShadow({ mode: 'open' });
+    
   }
-
+  
   connectedCallback() {
+    if(this.hasAttribute('tip-text')) {
+      this._tooltipText = this.getAttribute('tip-text');
+    }
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip);
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip = () => {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
-    this.appendChild(this._tooltipContainer);
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip = () => {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
