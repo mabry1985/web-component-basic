6857e14326ff37156cca34daad721b8cb5b83214
Josh Mabry <mabry1985@gmail.com>
2021-02-07 14:01:49 -0800

    styling

--- tooltip.js
+++ tooltip.js
@@ -7,11 +7,32 @@ class Tooltip extends HTMLElement {
     this.shadowRoot.innerHTML = `
       <style>
         div {
+          font-weight: normal;
           background-color: black;
           color: white;
           position: absolute;
+          top: 1.5rem;
+          left: 0.75rem;
           z-index: 10;
+          padding: 0.15rem;
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26)
         }
+
+        :host(.important) {
+          background: var(--color-primary, #ccc);
+          padding: 0.15rem
+        }
+
+        :host-context(p) {
+          font-weight: bold;
+        }
+
+        .highlight {
+        }
+
+        ::slotted(.highlight) {
+          border-bottom: 1px dotted red;        }
 
       </style>
       <slot>Some default</slot>
@@ -24,7 +45,7 @@ class Tooltip extends HTMLElement {
     if(this.hasAttribute('tip-text')) {
       this._tooltipText = this.getAttribute('tip-text');
     }
-    const tooltipIcon =this.shadowRoot.querySelector('span');
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip);
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip);
     this.shadowRoot.appendChild(tooltipIcon);
